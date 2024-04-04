a7bf298814f100377ef74b4f8d408fa47c121715
Pedro Arns Junior <pedroarnsjr@outlook.com>
2024-04-03 21:17:04 -0300

    Orientação a Objetos em Python

---
+++ Aula14/app.py
@@ -0,0 +1,17 @@
+from carro import Carro
+
+fusca = Carro("Volks","Fusca","Prata","Gasolina")
+uno = Carro("Ford","Fiat Uno","Preto","Gasolina")
+
+fusca.ligar()
+fusca.acelerar()
+fusca.frear()
+fusca.desligar()
+
+print("\n")
+
+uno.ligar()
+uno.acelerar()
+uno.frear()
+uno.desligar()
+
